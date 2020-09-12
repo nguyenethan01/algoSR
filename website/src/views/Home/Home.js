77c581222201166a35fdf8be9304441909f9d1dc
nguyenethan01 <nguyenethan01@gmail.com>
2020-09-12 09:26:32 -0700

    Barebones Card

--- website/src/views/Home/Home.js
+++ website/src/views/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import { BrowserRouter as Router} from 'react-router-dom';
+import Reviewcard from '../../components/ReviewCard/ReviewCard'
 
 function Home() {
   return (
-    <Router>
-      <NavBar />
-      <h1>Home</h1>
-    </Router>
+    <div className="home">
+      <Router>
+        <NavBar />
+        <h1>Home</h1>
+      </Router>
+      <Reviewcard />
+    </div>
   );
 }
 
