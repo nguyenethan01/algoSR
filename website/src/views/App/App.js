aef70ec7ac9481b16cce9a309ed315c22acf345f
nguyenethan01 <nguyenethan01@gmail.com>
2020-09-11 20:12:31 -0700

    Fixed routing to homepage

--- website/src/views/App/App.js
+++ website/src/views/App/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import './App.css';
 import Login from '../Login/Login';
 import SignUp from '../SignUp/SignUp';
+import Home from '../Home/Home';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Route path='/' exact component={Home} />
-      <Route path='/login' component={Login} />
-      <Route path='/signup' component={SignUp} />
+      <Switch>
+        <Route path='/' exact component={Landing} />
+        <Route path='/home' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={SignUp} />
+      </Switch>
     </Router>
   );
 }
 
-const Home = () => (
+const Landing = () => (
   <div>
-    <h1>Home Page</h1>
+    <h1>Landing Page</h1>
     <Link to='/signup'>
       <p> Sign Up </p>
     </Link>
