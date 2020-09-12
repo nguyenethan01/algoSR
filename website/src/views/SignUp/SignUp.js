75c6e40b2e3150a259f99e1389e8b300935d5077
Mingjia Wang <mingjiawangdev@gmail.com>
2020-09-12 10:06:35 -0700

    Add npm libraries

--- website/src/views/SignUp/SignUp.js
+++ website/src/views/SignUp/SignUp.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './SignUp.css';
 
 function SignUp() {
   return (
     <div className="signup">
       <h1> Sign Up </h1>
+      <form>
+        <label for="email">Email: </label>
+        <input type="text" id="email" name="email" placeholder="email"></input>
+
+        <label for="password">Password: </label>
+        <input type="password" id="password" name="password" placeholder="password"></input>
+        <button>Register</button>
+      </form>
       <Link to='/home'>
         <p> Home </p>
       </Link>
