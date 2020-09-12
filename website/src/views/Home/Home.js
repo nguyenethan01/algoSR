a54bf785375e279ac44d626061a181568305db31
nguyenethan01 <nguyenethan01@gmail.com>
2020-09-12 09:29:45 -0700

    Fix import error

--- website/src/views/Home/Home.js
+++ website/src/views/Home/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import ReviewCard from '../../components/ReviewCard/ReviewCard';
 import { BrowserRouter as Router} from 'react-router-dom';
-import Reviewcard from '../../components/ReviewCard/ReviewCard';
 
 function Home() {
   return (
