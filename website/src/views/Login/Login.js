b04ecdd5b5166b66b63403c453d95f2592fa128f
Mingjia Wang <mingjiawangdev@gmail.com>
2020-09-12 13:55:14 -0700

    Make login/signup form

--- website/src/views/Login/Login.js
+++ website/src/views/Login/Login.js
@@ -1,15 +1,102 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
 
-function Login() {
-  return (
-    <div className="login">
-      <h1> Login </h1>
-      <Link to='/home'>
-        <p> Home </p>
-      </Link>
-    </div>
-  );
+class Login extends Component {
+  constructor() {
+      super()
+      this.state = {
+          username: '',
+          password: '',
+          redirectTo: null
+      }
+      this.handleSubmit = this.handleSubmit.bind(this)
+      this.handleChange = this.handleChange.bind(this)
+
+  }
+
+  handleChange(event) {
+      this.setState({
+          [event.target.name]: event.target.value
+      })
+  }
+
+  handleSubmit(event) {
+      event.preventDefault()
+      console.log('handleSubmit')
+
+      axios
+          .post('/user/login', {
+              username: this.state.username,
+              password: this.state.password
+          })
+          .then(response => {
+              console.log('login response: ')
+              console.log(response)
+              if (response.status === 200) {
+                  // update App.js state
+                  this.props.updateUser({
+                      loggedIn: true,
+                      username: response.data.username
+                  })
+                  // update the state to redirect to home
+                  this.setState({
+                      redirectTo: '/'
+                  })
+              }
+          }).catch(error => {
+              console.log('login error: ')
+              console.log(error);
+              
+          })
+  }
+
+  render() {
+    return (
+      <div className="LoginForm">
+        <h1 id="heading">Log In</h1>
+        <form className="form-horizontal">
+          <div className="form-group">
+            <div className="col-1 col-ml-auto">
+              <label className="form-label" htmlFor="username">Username: </label>
+            </div>
+            <div className="col-3 col-mr-auto">
+              <input className="form-input"
+                type="text"
+                id="username"
+                name="username"
+                placeholder="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-1 col-ml-auto">
+              <label className="form-label" htmlFor="password">Password: </label>
+            </div>
+            <div className="col-3 col-mr-auto">
+              <input className="form-input"
+                placeholder="password"
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-group ">
+            <div className=""></div>
+            <button
+              className="button"
+              onClick={this.handleSubmit}
+              type="submit"
+            >Login</button>
+          </div>
+        </form>
+      </div>
+  
+    )
+  }
 }
-
 export default Login;
--- website/src/views/SignUp/SignUp.js
+++ website/src/views/SignUp/SignUp.js
@@ -1,15 +1,99 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import axios from 'axios';
+import './SignUp.css';
 
-function SignUp() {
-  return (
-    <div className="signup">
-      <h1> Sign Up </h1>
-      <Link to='/home'>
-        <p> Home </p>
-      </Link>
-    </div>
-  );
+class SignUp extends Component {
+	constructor() {
+		super()
+		this.state = {
+			username: '',
+			password: '',
+			confirmPassword: '',
+
+		}
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+	handleSubmit(event) {
+		console.log('sign-up handleSubmit, username: ')
+		console.log(this.state.username)
+		event.preventDefault()
+
+		//request to server to add a new username/password
+		axios.post('/user/', {
+			username: this.state.username,
+			password: this.state.password
+		})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg) {
+					console.log('successful signup')
+					this.setState({ //redirect to login page
+						redirectTo: '/login'
+					})
+				} else {
+					console.log('username already taken')
+				}
+			}).catch(error => {
+				console.log('signup error: ')
+				console.log(error)
+
+			})
+	}
+
+
+render() {
+	return (
+		<div className="SignupForm">
+			<h1 id="heading">Sign up</h1>
+			<form className="form-horizontal">
+				<div className="form-group">
+					<div className="col-1 col-ml-auto">
+						<label className="form-label" htmlFor="username">Username: </label>
+					</div>
+					<div className="col-3 col-mr-auto">
+						<input className="form-input"
+							type="text"
+							id="username"
+							name="username"
+							placeholder="username"
+							value={this.state.username}
+							onChange={this.handleChange}
+						/>
+					</div>
+				</div>
+				<div className="form-group">
+					<div className="col-1 col-ml-auto">
+						<label className="form-label" htmlFor="password">Password: </label>
+					</div>
+					<div className="col-3 col-mr-auto">
+						<input className="form-input"
+							placeholder="password"
+							type="password"
+							name="password"
+							value={this.state.password}
+							onChange={this.handleChange}
+						/>
+					</div>
+				</div>
+				<div className="form-group ">
+					<div className=""></div>
+					<button
+						className="button"
+						onClick={this.handleSubmit}
+						type="submit"
+					>Register</button>
+				</div>
+			</form>
+		</div>
+
+	)
+}
 }
 
 export default SignUp;
