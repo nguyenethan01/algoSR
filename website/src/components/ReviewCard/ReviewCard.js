0ad4a7a6c69fb41a6c78d99b3b71be03b0bfcc60
Mingjia Wang <mingjiawangdev@gmail.com>
2020-09-12 21:43:39 -0700

    Set up review card

--- website/src/components/ReviewCard/ReviewCard.js
+++ website/src/components/ReviewCard/ReviewCard.js
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ReviewCard.css'
 
-function ReviewCard(props){
+class ReviewCard extends Component {
 
-  return(
+  constructor(props) {
+
+    console.log(props)
+    super();
+    this.state = {
+      index: 0,
+      numOfCards: props.info.length,
+      questions: props.info
+    }
+
+    this.buttonClicked = this.buttonClicked.bind(this)
+  }
+
+  buttonClicked() {
+    this.setState({
+      index: this.state.index + 1
+    });
+  }
+
+  render() {
     
-    <div className="reviewCard">
-      <h1>
-        123. Two Sum
-      </h1>
-      <button className="btn-primary">Practice</button>
-      <p className="notes" >
-        <br/>
-        Notes:
-        <br/>
-        - Use a dictionary
-        <br/>
-        - O(n) runtime!
-      </p>
-      <div className="easy-medium-hard">
-        <button className="btn-success">Easy</button>
-        <button className="btn-warning">Medium</button>
-        <button className="btn-error">Hard</button>
-      </div>
-    </div>
-
-  );
+    if (this.state.index < this.state.numOfCards) {
+      return(
+        <div className="reviewCard">
+          <h1>
+            {this.state.questions[this.state.index].title}
+          </h1>
+          <button className="btn-primary">Practice</button>
+          <div className="notes" >
+            <p>Notes:</p>
+            <p>
+              {this.state.questions[this.state.index].comments}
+            </p>
+          </div>
+          <div className="easy-medium-hard">
+            <button onClick={this.buttonClicked} className="btn-success">Easy</button>
+            <button onClick={this.buttonClicked} className="btn-warning">Medium</button>
+            <button onClick={this.buttonClicked} className="btn-error">Hard</button>
+          </div>
+        </div>
+      );
+    } else {
+      return(
+        <div className="no-card">
+            <h2>You're all out of questions for today!</h2>
+        </div>
+      )
+    }
+  }
+ 
 } 
 export default ReviewCard 
