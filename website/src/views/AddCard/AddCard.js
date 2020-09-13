bb2c66da8cb5f2969d880e02887e1fd255b95e47
nguyenethan01 <nguyenethan01@gmail.com>
2020-09-12 21:18:38 -0700

    Create Add Card UI

--- website/src/views/AddCard/AddCard.js
+++ website/src/views/AddCard/AddCard.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
+import './AddCard.css';
 
 function AddCard(){
   return (
-    <div>
+    <div className = "add-page">
       <NavBar />
-      <h1>Add Card</h1>
+      <div className="add-card">
+      <div class="form-group">
+        <label class="form-label" for="input-example-1">Question Title</label>
+        <input class="form-input" type="text" id="input-example-1" placeholder="Title"></input>
+        <label class="form-label" for="input-example-1">Question URL</label>
+        <input class="form-input" type="text" id="input-example-1" placeholder="URL"></input>
+        <label class="form-label" for="input-example-1">Notes</label>
+        <input class="form-input" type="text" id="input-example-1" placeholder="Notes"></input>
+      </div>
+      <button type="button" className = "add-button">Submit</button>
+      </div>
     </div>
   );
 }
